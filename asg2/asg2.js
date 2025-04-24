78b6dc2c40cd75dc2001a5e7e3ca6629ce88a2c4
Amisha Kandi <amisha.edu@gmail.com>
2025-04-23 22:49:34 -0700

    almost done with asg2

--- asg2/asg2.js
+++ asg2/asg2.js
@@ -85,17 +85,16 @@ var g_cameraAngleX = 0.0;
 
 //Body parts
 let g_headYaw = 0;
-let g_flUpper = 10; 
+let g_flUpper = 0; 
 let g_flLower = 0;  
-let g_frUpper = 10;
+let g_frUpper = 0;
 let g_frLower = 0;
-let g_blUpper = -20;
-let g_blLower = 40;
-let g_brUpper = -20;
-let g_brLower = 40;
+let g_blUpper = 0;
+let g_blLower = 0;
+let g_brUpper = 0;
+let g_brLower = 0;
 let g_tailBase = 0;
-let g_tailMid = 0;
-let g_tailTip = 0; 
+let g_earTilt = 0;
 
 //Buttons
 let g_animationActive = true;
@@ -104,19 +103,20 @@ let g_shiftMode = false;
 //Mouse drag
 var g_deltaX = 0, g_deltaY = 0;
 
-//Performance related
-let g_startTime = performance.now()/1000.0;
-let g_seconds   = performance.now()/1000.0 - g_startTime;
-
 function addActionsForHtmlUI() {
 
     //Buttons 
     document.getElementById('toggle-animation').onclick = function() {g_animationActive = !g_animationActive;};
-    document.getElementById('toggle-shift').onclick = function() {g_shiftMode = !g_shiftMode;};
-
+    document.getElementById('toggle-shift').onclick = function () {
+        g_shiftMode = !g_shiftMode;
+        if (!g_shiftMode) {
+          g_cameraAngleX = 0;
+        }
+    };
     //Slider controls
     document.getElementById('angleSlide').addEventListener('mousemove', function() {g_cameraAngleY = this.value; renderAllShapes();});
     document.getElementById('headYaw').addEventListener('mousemove', function() {g_headYaw = this.value; renderAllShapes();});
+    document.getElementById('earTilt').addEventListener('mousemove', function() {g_earTilt = this.value; renderAllShapes();});
 
     //Body slider controls 
     document.getElementById('flUpper').addEventListener('mousemove', function() {g_flUpper = this.value; renderAllShapes();});
@@ -132,9 +132,10 @@ function addActionsForHtmlUI() {
     document.getElementById('brLower').addEventListener('mousemove', function() {g_brLower = this.value; renderAllShapes();});
 
 
-    document.getElementById('canvas-container').addEventListener('click', function(ev) {
-        if(ev.shiftKey) {
-            g_shiftMode = !g_shiftMode;
+    document.getElementById('canvas-container').addEventListener('click', ev => {
+        if (ev.shiftKey) {
+          g_shiftMode = !g_shiftMode;
+          if (!g_shiftMode) g_cameraAngleX = 0;
         }
     });
 
@@ -163,6 +164,34 @@ function convertMouseToEventCoords(ev) {
   
     return([x, y]);
 }
+
+function updateAnimationAngles() {
+    if (!g_animationActive) return;
+    const w = g_seconds * 3.3;
+  
+    /* idle walk */
+    g_flUpper =  15 * Math.sin(w);  g_flLower = 25 * Math.sin(w);
+    g_frUpper = -15 * Math.sin(w);  g_frLower = -25 * Math.sin(w);
+    g_blUpper = -10 * Math.sin(w);  g_blLower = 20 * Math.sin(w);
+    g_brUpper =  10 * Math.sin(w);  g_brLower = -20 * Math.sin(w);
+    g_tailBase = 15 * Math.sin(w);      
+    g_earTilt  =  8 * Math.sin(w * 1.7);
+  
+    //--- roll onto back & wiggle ---
+    if (g_shiftMode) {
+      const wig = 20 * Math.sin(w * 5); //paw wiggle
+      g_cameraAngleX = 90;
+  
+      g_flUpper = 60 + wig;   g_flLower = -wig;
+      g_frUpper = 60 - wig;   g_frLower =  wig;
+      g_blUpper = 60 - wig;   g_blLower = -wig;
+      g_brUpper = 60 + wig;   g_brLower =  wig;
+  
+      g_tailBase = 0;
+      g_earTilt  = 0;
+    } 
+  }
+  
   
 
 //Draw every shape that is supposed to be in the canvas
@@ -176,18 +205,17 @@ function renderAllShapes(){
 
     //Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    //Draw the body cube (starts as an identity matrix, scale, then translate due to right multiply)
     
     /* ---------- torso ---------- */
     var torso = new Cube();
-    torso.color = [0.52, 0.333, 0.004, 1];           
+    torso.color = [0.431, 0.247, 0.122, 1];           
     torso.matrix.translate(-0.55,-0.15, 0);
     torso.matrix.scale(0.75, 0.3, 0.50);
     torso.render();
 
     /* ---------- head area ---------- */
     var neck = new Cube();
-    neck.color = [0.52, 0.333, 0.004, 1]; 
+    neck.color = [0.431, 0.247, 0.122, 1]; 
     neck.matrix.translate(-0.058, -0.05, 0.15);
     neck.matrix.rotate(-10,0,0,1);
     neck.matrix.rotate(g_headYaw, 0,1,0);
@@ -195,44 +223,48 @@ function renderAllShapes(){
     neck.render();
 
     var head = new Cube();
-    head.color = [0.659, 0.447, 0.047, 1];
+    head.color = [0.49, 0.302, 0.173, 1];
     head.matrix.translate(-0.008, 0.332, 0.115);
     head.matrix.rotate(-11,0,0,1);
     head.matrix.rotate(g_headYaw, 0,1,0);
+    var head_mat = new Matrix4(head.matrix);
     head.matrix.scale(0.38, 0.23, 0.26);
     head.render();
 
     var e1 = new Pyramid();
     e1.color = [0.361, 0.224, 0.031,1];
-    e1.matrix.translate(0.004,0.555,0.05);
+    e1.matrix = new Matrix4(head_mat);
+    e1.matrix.translate(0.004,0.24,0.15);
     e1.matrix.rotate(-10,0,0,1);
-    e1.matrix.rotate(g_headYaw, 0,1,0);
-    e1.matrix.scale(0.13, 0.13, 0.15);
+    e1.matrix.rotate(g_earTilt, 1,0,0);
+    e1.matrix.scale(0.11, 0.11, 0.13);
     e1.render();
 
     var e2 = new Pyramid();
     e2.color = [0.361, 0.224, 0.031, 1];
-    e2.matrix.translate(0.003, 0.555, 0.25);
+    e2.matrix = new Matrix4(head_mat);
+    e2.matrix.translate(0.003, 0.24, 0.10);
     e2.matrix.rotate(-10, 0, 0, 1);       
-    e2.matrix.rotate(g_headYaw, 0, 1, 0);  
-    e2.matrix.scale(0.13, 0.13, 0.15);
+    e2.matrix.rotate(-g_earTilt, 1, 0, 0);  
+    e2.matrix.scale(0.11, 0.11, -0.13);
     e2.render();
 
-    
+    /* ---------- tail ---------- */
+
+    var t = new Cube();
+    t.color = [0.361, 0.224, 0.031, 1];
+    t.matrix.translate(-0.66, -0.05, 0.21); 
+    t.matrix.rotate(g_tailBase, 0, 1, 0);
+    t.matrix.rotate(5, 0, 0, 1);
+    t.matrix.scale(0.11, 0.11, 0.11);
+    t.render();
 
 
-    // /* ---------- 2. three-segment tail ---------- */
-    // const tailColor = [0.45, 0.38, 0.23, 1];
-    // tailSegment(-0.28, 0.04,  0.16, g_tailBase, 0.09, 0.15, tailColor); // base
-    // tailSegment(-0.28, 0.04,  0.31, g_tailMid , 0.07, 0.13, tailColor); // mid
-    // tailSegment(-0.28, 0.04,  0.43, g_tailTip , 0.05, 0.11,[0.75,0.75,0.75,1]); // tip
-
-
-    /* ---------- 4. front-left leg (short & centred) ---------- */
-    leg(0.01,-0.07,0.08, g_flUpper, g_flLower, [0.659, 0.447, 0.047, 1], [0.361, 0.224, 0.031, 1]);
-    leg(0.01, -0.07, 0.35, g_frUpper, g_frLower, [0.659, 0.447, 0.047, 1],[0.361, 0.224, 0.031, 1]);
-    // leg( 0.10, -0.04,  0.18, g_blUpper, g_blLower, [0.659, 0.447, 0.047, 1],[0.361, 0.224, 0.031, 1]);
-    // leg(-0.25, -0.04,  0.18, g_brUpper, g_brLower, [0.659, 0.447, 0.047, 1],[0.361, 0.224, 0.031, 1]);
+    /* ---------- legs ---------- */
+    leg(0.01,-0.07,0.08, g_flUpper, g_flLower, [0.49, 0.302, 0.173, 1], [0.361, 0.224, 0.031, 1]);
+    leg(0.01, -0.07, 0.35, g_frUpper, g_frLower, [0.49, 0.302, 0.173, 1],[0.361, 0.224, 0.031, 1]);
+    leg(-0.5, -0.08,  0.08, g_blUpper, g_blLower, [0.49, 0.302, 0.173, 1],[0.361, 0.224, 0.031, 1]);
+    leg(-0.50, -0.08,  0.35, g_brUpper, g_brLower, [0.49, 0.302, 0.173, 1],[0.361, 0.224, 0.031, 1]);
 
 
     var duration = performance.now() - start_time;
@@ -255,7 +287,7 @@ function leg(offsetX, offsetY, offsetZ, upperAng, lowerAng, legColor, pawColor){
     var upper = new Cube();
     upper.color = legColor;
     upper.matrix.setTranslate(offsetX, offsetY, offsetZ);
-    upper.matrix.rotate(upperAng, 1,0,0);
+    upper.matrix.rotate(upperAng, 0,0,1);
     var saveUpper = new Matrix4(upper.matrix);
     upper.matrix.rotate(180,1,0,0);
     upper.matrix.scale(0.12,0.21,-0.12);
@@ -267,7 +299,7 @@ function leg(offsetX, offsetY, offsetZ, upperAng, lowerAng, legColor, pawColor){
     lower.matrix = saveUpper;
     lower.matrix.translate(0.015,-0.19,0);
     lower.matrix.rotate(180,1,0,0);
-    lower.matrix.rotate(lowerAng,1,0,0);
+    lower.matrix.rotate(lowerAng,0,0,1);
     var saveLower = new Matrix4(lower.matrix);
     lower.matrix.scale(0.09,0.15,-0.09);
     lower.render();
@@ -307,9 +339,14 @@ function main() {
     requestAnimationFrame(tick);
 }
 
+var g_startTime = performance.now()/1000.0;
+var g_seconds = performance.now()/1000.0 - g_startTime;
+
 function tick(){
     //Get the current time
     g_seconds = performance.now()/1000.0 - g_startTime;
+
+    updateAnimationAngles();
 
     renderAllShapes();
 
